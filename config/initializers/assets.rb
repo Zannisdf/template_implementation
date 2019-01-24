e2f9311424637a2593b0a0032972821631e0ff4c
Giorgio Zanni <g.zanni@hotmail.com>
2019-01-24 18:19:39 -0300

    Implement template to project

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,10 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+# Rails.application.config.assets.precompile += %w( jquery.js jquery.easing.js bootstrap.bundle.js new-age.js )
+Rails.application.config.assets.precompile += %w( *.eot *.svg *.ttf *.woff *.woff2 *.otf )
